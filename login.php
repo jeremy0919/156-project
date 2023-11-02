9b2f3cc8bddd09e06d48a477cb0d91601ce99de4
Jeremy Davis <jeremy0919@icloud.com>
2023-11-01 17:54:52 -0700

    update

--- login.php
+++ login.php
@@ -1,5 +1,29 @@
 <?php // start at log in page, log in page goes to selection of one or two player
 // Read the existing JSON file
+
+if (isset($_POST['submit'])) {
+    $json_file = 'database.json';
+    $json_data = file_get_contents($json_file);
+    $data = json_decode($json_data, true);
+
+    $name = trim($_POST['username']);
+    
+    if (isset($data['User']) && is_array($data['User'])) {
+        foreach ($data['User'] as $key => $user) {
+            if ($user['name'] == $name) {
+                echo "{$key} = {$user['name']} <br>";
+                echo "{$key} = {$user['gamesPlayed']} <br>";
+                echo "{$key} = {$user['wins']} <br>";
+                echo "{$key} = {$user['losses']} <br>";
+                echo "{$key} = {$user['winRate']} <br>";
+            }
+        }
+    }
+
+    echo $name;
+}
+
+/*
     if(isset($_POST['submit'])){
     $json_file = 'database.json';
     $json_data = file_get_contents($json_file);
@@ -16,6 +40,19 @@
 // Get data from the form
 
 
+$data = json_decode($jsonData, true);
+
+if (isset($data['User']) && is_array($data['User'])) {
+    $nameToSearch = "jeremy"; // Change this to the name you want to search for
+
+    foreach ($data['User'] as $user) {
+        $name = $user['name'];
+        if ($name == $nameToSearch) {
+            echo "{$name} <br>";
+        }
+    }
+}
+*/
 // Encode the updated data back to JSON
 //$updated_json = json_encode($users, JSON_PRETTY_PRINT);
 
