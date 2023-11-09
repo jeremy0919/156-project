f6983ad649ca9a0b96898dc55a397fb83e96ba32
jeremy0919 <jeremy0919@icloud.com>
2023-11-09 11:50:01 -0800

    update

---
+++ webTests/socket.php
@@ -0,0 +1,47 @@
+<?php
+require 'vendor/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\WebSocket\WsServer;
+use Ratchet\Http\HttpServer;
+
+class TicTacToeServer implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($client !== $from) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new TicTacToeServer()
+        )
+    ),
+    8080
+);
+
+$server->run();
+?>
