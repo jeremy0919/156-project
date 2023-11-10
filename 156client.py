59a495f11454da55fb87f65240bc3cd6f0fe6a11
JohnnyCake-21 <141998620+JohnnyCake-21@users.noreply.github.com>
2023-11-10 10:37:41 -0800

    Add files via upload

---
+++ 156client.py
@@ -0,0 +1,18 @@
+import socket
+
+
+HEADER = 64 #header is the same
+PORT = 60061 #ports have to match
+SERVER = "localhost" #"192.168.56.1"  #client server is this
+ADDR = (SERVER, PORT)
+FORMAT = 'uft-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.bind(ADDR)
+client.connect((SERVER, 60060))
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
---
+++ 156server.py
@@ -0,0 +1,43 @@
+import socket
+import threading
+import time
+
+
+HEADER = 64
+PORT = 60060 #ports over 4000 (sike, my ports are up to 6k) are typically inactive or unused
+#SERVER = '192.168.1.22"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'uft-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print("[NEW CONNECTION]{addr} connected.") 
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int (msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}] {msg}")
+    
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args = (conn,addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount()-1}")
+
+
+print("[STARTING] server is starting")
+start()
