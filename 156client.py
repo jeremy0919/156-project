77f675c9cd38d6768fc19acee3c2f3eda2b030ee
Jeremy Davis <jeremy0919@icloud.com>
2023-11-10 11:55:34 -0800

    update

--- 156client.py
+++ 156client.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 
 HEADER = 64 #header is the same
-PORT = 60061 #ports have to match
+PORT = 7000 #ports have to match
 SERVER = "localhost" #"192.168.56.1"  #client server is this
 ADDR = (SERVER, PORT)
 FORMAT = 'UTF-8'
@@ -12,7 +12,7 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 try:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.bind(ADDR)
-    client.connect((SERVER, 60060))
+    client.connect(SERVER, 7000)
 except socket.error as e:
     print(f"Error creating/connecting socket: {e}")
     exit(1)
