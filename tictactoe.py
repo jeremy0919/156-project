f00db59fa0dff9b2a9142a737ad9b6c884a5f7f4
JohnnyCake-21 <141998620+JohnnyCake-21@users.noreply.github.com>
2023-11-28 14:13:52 -0800

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,17 +1,31 @@
 class TicTacToe:
 
     def __init__(self):
-        self.board = [' ' for _ in range(9)]  # we will use a single list to rep 3x3 board
+        self.board = [' ' for _ in range(10)]  # we will use a single list to rep 3x3 board
         self.player_symbol = ['X', 'O']   # keep track of which player goes first
         self.win_combos = [    # all possible winning combos
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8]
+            
+             # #off of 1-9 gives a zero error
+             [1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9],
+             [1, 5, 9],
+             [3, 5, 7],
+             [1, 4, 7],
+             [2, 5, 8],
+             [3, 6, 9]
+            
+            # #off of 0-8 gives error with 9(8)
+            #  [0, 1, 2],
+            #  [3, 4, 5],
+            #  [6, 7, 8],
+            #  [0, 4, 8],
+            #  [2, 4, 6],
+            #  [0, 3, 6],
+            #  [1, 4, 7],
+            #  [2, 5, 8]
+            
+        
         ]
         self.player = 0    # keep track of winner
 
@@ -24,7 +38,7 @@ class TicTacToe:
     def validMove(self, move, player):
         return 1 <= move <= 9 and self.board[move] == ' '
     def makeMove(self, move, player):
-        self.board[move] = self.player_symbol[player - 1]
+        self.board[move - 1] = self.player_symbol[player - 1]
         self.player = self.go2OthPlayer() 
     def isWinner(self, player):                        # check if player has won
         for combo in self.win_combos:
@@ -36,7 +50,7 @@ class TicTacToe:
         return all(i != ' ' for i in self.board)         # check if board is full
     
     def restart(self):
-        self.board = [' ' for _ in range(9)]
+        self.board = [' ' for _ in range(10)]
         self.player = 0
     def show(self):
         #print("-------------")
